36d748bfd65afc649ed2b638a660640741b5339b
Valdir Luiz <arnholdvaldir@gmail.com>
2017-09-07 19:40:36 -0300

    add níveis de dificuldade

--- js/app.js
+++ js/app.js
@@ -1,25 +1,42 @@
 
-
+var mode = 0;
+var isRunning = false;
 
 var _showResult = function (state) {
     if (state.isTerminal()) {
         if (state.result === 'DRAW') {
-            Materialize.toast('Deu empate!', 40000)
+            Materialize.toast('Deu empate!', 4000)
         } else {
-            if(state.result==='O'){
-                Materialize.toast('Bob ganhou!', 40000);
-            }else{
-                Materialize.toast('Você ganhou!', 40000);
+            if (state.result === 'O') {
+                Materialize.toast('Bob ganhou!', 4000);
+            } else {
+                Materialize.toast('Você ganhou!', 4000);
             }
         }
-
+        isRunning = false;
     }
 };
 
-var _aiAction = function (state, aiPlayer) {
-    var aiPosition = aiMove(state);
-    state.board[aiPosition] = aiPlayer;
+var _aiAction = function (state, aiPlayer, mode) {
 
+    var aiPosition;
+
+    if (mode == 0) {
+        var movePosition = Math.floor(Math.random() * state.moves().length);
+        aiPosition = state.moves()[movePosition];
+    } if (mode == 1) {
+        if (Math.random() * 100 <= 40) {
+            aiPosition = aiMove(state);
+        } else {
+            var movePosition = Math.floor(Math.random() * state.moves().length);
+            aiPosition = state.moves()[movePosition];
+        };
+    } else if (mode == 2) {
+        aiPosition = aiMove(state);
+    };
+
+
+    state.board[aiPosition] = aiPlayer;
     $(".item").each(function (index) {
         if (aiPosition == index) {
             $(this).addClass("selected-pc");
@@ -31,15 +48,26 @@ var _aiAction = function (state, aiPlayer) {
 };
 
 
+
+
 $(document).ready(function () {
 
+    $('select').material_select();
+
+    
     var state = new State();
     var player = 'X';
     var aiPlayer = 'O';
 
     $('.item').click(function (e) {
         if (!state.isTerminal()) {
-
+            
+            if(!isRunning){
+                isRunning = true;
+                $('#mode').prop('disabled', true);
+                $('select').material_select();
+            };
+            
             $(this).addClass("selected-human");
             var position = $(this).attr('data-pos');
             state.board[position] = player;
@@ -47,17 +75,25 @@ $(document).ready(function () {
             if (state.isTerminal()) {
                 _showResult(state);
             } else {
-                setTimeout(function () { _aiAction(state, aiPlayer); }, 500);
+                setTimeout(function () { _aiAction(state, aiPlayer, mode); }, 500);
             };
 
         }
     });
 
     $("#reset").click(function (e) {
+        isRunning = false;
+        $('#mode').prop('disabled', false);
+        $('select').material_select();
+
         state.board = state.board.map(function () { return "E"; });
         $(".item").each(function (index) {
             $(this).removeClass("selected-human selected-pc")
         });
+    });
+
+    $('#mode').on('change', function () {
+       mode = parseInt(this.value);
     });
 
 
--- js/minmax.js
+++ js/minmax.js
@@ -1,60 +1,59 @@
 
 function minmax(state) {
-    if (state.isTerminal() === true) {
-      if (state.result === state.playerSymbol) {
-        return  -10; 
-      } else if (state.result === state.aiSymbol) {  
-        return 10;
-      } else {
-        return 0;
-      }
-    } 
-    
-    // Generate list of possible new game states (moves) 
-    newStatesSet = state.moves().map(function (el) {
-      var newState = new State(state);
-      newState.board[el] = state.turn.slice(0);
-      newState.advanceTurn();
-      newState.element = el;
-      return newState;
-    });  
-    // Array to hold all child scores
-    var newStateScores = [];
-  
-    // For each of these states, add the minmax score of 
-    // that state to the scoreList
-    newStatesSet.forEach(function(newState) {
-      var newStateScore = minmax(newState);
-      newStateScores.push(newStateScore);
-    });
-    if (state.turn === state.playerSymbol) {
-      stateScore = Math.min(...newStateScores);
+  if (state.isTerminal() === true) {
+    if (state.result === state.playerSymbol) {
+      return -10;
+    } else if (state.result === state.aiSymbol) {
+      return 10;
     } else {
-      stateScore = Math.max(...newStateScores);
+      return 0;
     }
-    return stateScore;
   }
-  
-  function aiMove(state) {
-    var possibleScores = [];
-    var possibleMoves = [];
-    var possibleStates = state.moves().map(function(el) {
-      var newState = new State(state);
-      possibleMoves.push(el);
-      newState.board[el] = state.aiSymbol;
-      possibleScores.push(minmax(newState));
-      return newState;
+
+  // Generate list of possible new game states (moves) 
+  newStatesSet = state.moves().map(function (el) {
+    var newState = new State(state);
+    newState.board[el] = state.turn.slice(0);
+    newState.advanceTurn();
+    newState.element = el;
+    return newState;
+  });
+  // Array to hold all child scores
+  var newStateScores = [];
+
+  // For each of these states, add the minmax score of 
+  // that state to the scoreList
+  newStatesSet.forEach(function (newState) {
+    var newStateScore = minmax(newState);
+    newStateScores.push(newStateScore);
+  });
+  if (state.turn === state.playerSymbol) {
+    stateScore = Math.min(...newStateScores);
+  } else {
+    stateScore = Math.max(...newStateScores);
+  }
+  return stateScore;
+};
+
+function aiMove(state) {
+  var possibleScores = [];
+  var possibleMoves = [];
+  var possibleStates = state.moves().map(function (el) {
+    var newState = new State(state);
+    possibleMoves.push(el);
+    newState.board[el] = state.aiSymbol;
+    possibleScores.push(minmax(newState));
+    return newState;
+  });
+  if (possibleMoves.length < 1) {
+    return -1;
+  }
+  function indexOfMax(arr) {
+    var max = arr.reduce(function (a, b) {
+      return b > a ? b : a;
     });
-    if (possibleMoves.length < 1) {
-      return -1;
-    }
-    function indexOfMax(arr) {
-      var max = arr.reduce(function(a,b) {
-        return b > a ? b : a;   
-      });
-      return arr.indexOf(max);
-    }
-    return possibleMoves[indexOfMax(possibleScores)];
-  }  
-  
-   
+    return arr.indexOf(max);
+  }
+  return possibleMoves[indexOfMax(possibleScores)];
+};
+
